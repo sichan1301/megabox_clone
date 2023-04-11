98f01a16a2b8dadbdce165b48994a343e0061e0c
sichan <sichan1301@naver.com>
2023-04-12 01:09:29 +0900

    화요일 집 작업

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import './App.css';
 import Header from './header/header';
 import Main from './main/main';
 import Footer from './footer/footer';
+import styled from 'styled-components';
+import Navbar from './navbar/navbar';
 
 function App() {
   return (
-    <>
+    <Wrap>
       <Header />
+      <Navbar />
       <Main />
       <Footer />
-    </>
+    </Wrap>
   );
 }
 
 export default App;
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
--- src/header/header.tsx
+++ src/header/header.tsx
@@ -3,9 +3,22 @@ import styled from "styled-components"
 
 const Header = () => {
   return(
-    <>
-      header
-    </>
+    <HeaderContainer>
+      <HeaderInner>
+
+        <HeaderUl>
+          <li>VIP LOUNGE</li>
+          <li>멤버십</li>
+          <li>고객센터</li>
+        </HeaderUl>
+        <HeaderUl>
+          <li>로그인</li>
+          <li>회원가입</li>
+          <li>빠른예매</li>
+        </HeaderUl>
+      </HeaderInner>
+
+    </HeaderContainer>
   )
 }
 
@@ -14,4 +27,22 @@ const Header = () => {
 
 export default Header
 
+const HeaderContainer = styled.div`
+  background-color: #08010a;
+  width:100%;
+`
 
+const HeaderInner = styled.header`
+  width:80%;
+  margin:0 auto;
+  display: flex;
+  justify-content: space-between;
+`
+
+const HeaderUl = styled.ul`
+  display:flex;
+  li{
+    margin-right:10px;
+    color:#6b6b6b;
+  }
+`
