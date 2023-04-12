58a549fdf07d0e5642a5f6918ea4e1c65f922671
sichan <sichan1301@naver.com>
2023-04-13 01:07:55 +0900

    수요일 집 작업

--- src/footer/footer.tsx
+++ src/footer/footer.tsx
@@ -1,13 +1,68 @@
 import styled from "styled-components"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
   return(
-    <>
-      footer
-    </>
+    <FooterSection>
+      <FooterMenu>
+        <ul>
+          <li>회사소개</li>
+          <li>인재채용</li>
+          <li>사회공헌</li>
+          <li>제휴/광고/부대사업문의</li>
+          <li>이용약관</li>
+          <li>위치기반서비스 이용약관</li>
+          <li>개인정보처리방침</li>
+          <li>윤리경영</li>
+        </ul>
+
+        <FindTheater>
+          <Icon icon={faMagnifyingGlass} />
+          <p>극장 찾기</p>
+        </FindTheater>
+      </FooterMenu>
+    </FooterSection>
   )
 }
 
 export default Footer
 
+const FooterSection = styled.footer`
+  padding: 20px 20% 40px;  
+  width:100%;
+`
 
+const FooterMenu = styled.div`
+  display:flex;
+  justify-content: space-between; 
+  ul{
+    padding:0;
+    display: flex;
+    li{
+      font-size: 12px;
+      margin-right:5px;
+      cursor:pointer;
+    }
+  }
+`
+
+const FindTheater = styled.div`
+  display:flex;  
+  align-items: center;
+  border:0.5px solid grey;
+  border-radius: 40px;
+  padding:2px 10px;
+  p{
+    color:rgb(110, 110, 110);
+    font-size:12px;
+    margin:0;
+  }
+`
+
+const Icon = styled(FontAwesomeIcon)`
+  font-size: 12px;
+  cursor: pointer;
+  margin-right: 10px;
+  color:rgb(110, 110, 110);
+`;
--- src/main/curation/curationSample.tsx
+++ src/main/curation/curationSample.tsx
@@ -4,6 +4,10 @@ const CurationSample = () => {
   return(
     <CurationSampleDiv>
       <img src = {require('../../imgs/cost.jpg')} />
+      <ButtonDiv>
+        <button>상세 정보</button>
+        <button>예매</button>
+      </ButtonDiv>
     </CurationSampleDiv>
   )
 }
@@ -14,10 +18,29 @@ export default CurationSample
 const CurationSampleDiv = styled.div`
   width:700px;
   height:500px;
-
+  margin-right:20px;
   img{
     border-radius:10px;
     width:100%;
     height:100%;
-  }
+    margin-bottom: 10px;
+  } 
 `
+
+const ButtonDiv = styled.div`
+  button{
+    width:49%;
+    height:50px;
+    border-radius: 10px;
+    :nth-child(1){
+      background-color: transparent;
+      margin-right:5px;
+      color:#fff;
+      border:1px solid #fff;
+    }
+    :nth-child(2){
+      border:none;
+      background-color: rgb(75, 183, 233);
+    }
+  }
+  `
